a0c2533b2cc745b5fdaa4a419975368da12ffd85
darkcoder7 <darlcoder7@safe-mail.net>
2015-08-03 02:41:19 +0000

    torrc

---
+++ fire.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+iptables -t nat -I OUTPUT -p tcp -m owner --uid-owner 106 --dport 80 -j DNAT --to-destination 127.0.0.1:8080
+iptables -t nat -I OUTPUT -p tcp -m owner --uid-owner 106 --dport 443 -j DNAT --to-destination 127.0.0.1:8080
